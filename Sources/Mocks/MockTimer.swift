668651e7d461135ff13d1f4ae2ce0bf3f92f5bc0
Hamed Hosseini <hamed8080@gmail.com>
2023-04-26 07:54:02 +0330

    - First Commit

---
+++ Sources/Mocks/MockTimer.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Additive
+
+open class MockTimer: TimerProtocol {
+    public var blockResult: ((TimerProtocol) -> Void)?
+    public var isValid: Bool = false
+
+    public init() {}
+
+    public func scheduledTimer(interval: TimeInterval, repeats: Bool, block: @escaping @Sendable (TimerProtocol) -> Void) -> TimerProtocol {
+        isValid = true
+        if let blockResult {
+            blockResult(self)
+            return self
+        }
+        self.blockResult = block
+        return self
+    }
+
+    public func invalidateTimer() {
+        isValid = false
+    }
+
+    public func fire() {
+        blockResult?(self)
+    }
+}
---
+++ Sources/Mocks/Mocks.docc/Mocks.md
@@ -0,0 +1,6 @@
+# ``Mocks``
+
+Mocks is consist of protocols that force you to use them for injection in your classes for the soul of unit-testing.
+
+![icon](icon.png)
+
