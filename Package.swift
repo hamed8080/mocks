723121790e3646283713bde625dfddd5e3065ede
Hamed Hosseini <hamed8080@gmail.com>
2024-05-20 12:52:52 +0330

    Versioning: Bump up the version to 1.2.1
    Change the git base URL

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["Mocks"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/additive", from: "1.2.0"),
+        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.1"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
